2367613c23c75ff8d4e269064b6432bea513077f
Austin Salonen <salonen@gmail.com>
2020-12-02 12:21:07 -0600

    :christmas_tree: 2020 Day 02
    Part 1:  Check character count range with Counter.
    Part 2:  XOR the char at 2 positions

---
+++ aoc2020/day02.py
@@ -0,0 +1,30 @@
+import re
+from collections import Counter
+
+def parse(line):
+	[rng, c, pwd] = [w for w in re.split(' |:|\n', line) if w != '']
+	[mn, mx] = rng.split('-')
+	return (int(mn), int(mx), c, pwd)
+
+def input(path):
+	with open(path) as f:
+		return [parse(line) for line in f.readlines()]
+
+def in_range(cs, mn, mx):
+	return cs >= mn and cs <= mx
+
+def min_max_chars(pwds):
+	return sum([1 for mn, mx, c, pwd in pwds if in_range(Counter(pwd)[c], mn, mx)])
+
+def char_in_exactly_one_of_the_positions(pwds):
+	return sum([1 for p0, p1, c, pwd in pwds if (pwd[p0-1] == c) ^ (pwd[p1-1] == c)])
+
+test_input = input('day02.test')
+real_input = input('day02.input')
+
+assert 2 == min_max_chars(test_input)
+assert 1 == char_in_exactly_one_of_the_positions(test_input)
+
+print('part 1 = ', min_max_chars(real_input))
+print('part 2 = ', char_in_exactly_one_of_the_positions(real_input))
+
